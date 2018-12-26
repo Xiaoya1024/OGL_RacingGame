531fabef142139f099c3c172ae132266fc95c8d2
abogiegie <abogiegie@163.com>
2018-12-26 09:46:20 +0800

    ppp

---
+++ OGL_RacingGame/Road.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"Vector.h"
+#include <vector>
+#include "Matrix.h"
+class Road
+{
+public:
+	Vector pos;//÷–µ„µƒŒª÷√111
+	float size;//≥ﬂ¥Á
+	Vector left;//◊Û±ﬂ’œ∞≠
+	Vector right;//”“±ﬂ’œ∞≠
+	void Setobs(float obs_size,Vector dir=Vector(0,0,1));//…Ë÷√’œ∞≠
+	void Setboundary(Vector dir);//…Ë÷√±ﬂΩÁ
+};
+void Setpos(float size);
+void Set_all_boundary(int begin,int end);
+void Set_Road();
