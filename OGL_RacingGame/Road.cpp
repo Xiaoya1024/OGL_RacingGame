dccf8ff1d20ba8c5351f4380ac6d71337c487de7
abogiegie <abogiegie@163.com>
2018-12-26 10:23:16 +0800

    路类

--- OGL_RacingGame/Road.cpp
+++ OGL_RacingGame/Road.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include "Road.h"
+#include <GL/glut.h>
 
 const float Pi = 3.14159265358979323846;
 float linewidth = 1;
 
-const int POINTNUM=425;
-Road point[POINTNUM];
-
-///////////////////////////rrr
-void Setpos(float size)//设置路线
+///////////////////////////
+void Road::Setpos(float size)//设置路线
 {
 	int i=0;
 	float x=0,y=0,z=0;
@@ -19,7 +17,6 @@ void Setpos(float size)//设置路线
 		point[i].size=width*size;
 		i++;
 	}
-	printf("%d\n",i);
 	for(float angle=0;angle<Pi/2;angle+=Pi/180)//90度右转(0,0,100)->(50,0,150)
 	{
 		x=50-50*cos(angle);
@@ -28,7 +25,6 @@ void Setpos(float size)//设置路线
 		point[i].size=width*size;
 		i++;
 	}
-	printf("%d\n",i);
 	for(float angle=0;angle<Pi/2;angle+=Pi/180)//90度左转(50,0,150)->(80,0,200)
 	{
 		x=50+30*sin(angle);
@@ -37,14 +33,12 @@ void Setpos(float size)//设置路线
 		point[i].size=width*size;
 		i++;
 	}
-	printf("%d\n",i);
 	for(x=80,z=200;z<300;z++)//菱形车道(80,0,200)->(80,0,300)
 	{
 		point[i].pos.Set(x*size,y*size,z*size);
 		point[i].size=width*size;
 		i++;
 	}
-	printf("%d\n",i);
 	for(float angle=0;angle<Pi/4;angle+=Pi/180)//45度左转(80,0,300)->(50+15*√2,0,300+15*√2)
 	{
 		x=50+30*cos(angle);
@@ -53,23 +47,23 @@ void Setpos(float size)//设置路线
 		point[i].size=width*size;
 		i++;
 	}
-	printf("%d\n",i);
 	/*for(float angle;angle<Pi/4;angle+=Pi/180)//90度右转(50+15*√2,0,300+15*√2)->()
 	{
 		x=
 	}*/
+	point_number=i;
 }
-void Set_all_boundary(int begin,int end)//传入起点和终点的点序号
+void Road::Set_all_boundary()
 {
-	for(int i=begin;i<end-1;i++)
+	for(int i=0;i<point_number-1;i++)
 	{
 		Vector road_dir(point[i+1].pos-point[i].pos);
 		point[i].Setboundary(road_dir);
 	}
-	Vector road_dir(point[end-1].pos-point[end-2].pos);
-	point[end-1].Setboundary(road_dir);
+	Vector road_dir(point[point_number-1].pos-point[point_number-2].pos);
+	point[point_number-1].Setboundary(road_dir);
 }
-void Road::Setboundary(Vector dir)//设置边界
+void Road_Point::Setboundary(Vector dir)//设置边界
 {
 	Vector temp;
 	dir.normalize();
@@ -80,8 +74,15 @@ void Road::Setboundary(Vector dir)//设置边界
 	this->left=temp * size + pos;
 	this->right=temp * -size + pos;
 }
-void Set_Road()
+void Road::initRoad()//设置路面
 {
-	Setpos(1.0f);
-	Set_all_boundary(0,POINTNUM);
+	for(int i=0;i<point_number;i++)
+	{
+		glBegin(GL_QUADS);
+		glVertex3f(point[i].left.x,point[i].left.y,point[i].left.z);
+		glVertex3f(point[i+1].left.x,point[i+1].left.y,point[i+1].left.z);
+		glVertex3f(point[i+1].right.x,point[i+1].right.y,point[i+1].right.z);
+		glVertex3f(point[i].right.x,point[i].right.y,point[i].right.z);
+		glEnd();
+	}
 }
--- OGL_RacingGame/Road.h
+++ OGL_RacingGame/Road.h
@@ -2,16 +2,27 @@
 #include"Vector.h"
 #include <vector>
 #include "Matrix.h"
-class Road
+
+const int MAX=10000;
+
+class Road_Point
 {
 public:
-	Vector pos;//中点的位置111
+	Vector pos;//中点的位置
 	float size;//尺寸
 	Vector left;//左边障碍
 	Vector right;//右边障碍
 	void Setobs(float obs_size,Vector dir=Vector(0,0,1));//设置障碍
 	void Setboundary(Vector dir);//设置边界
 };
-void Setpos(float size);
-void Set_all_boundary(int begin,int end);
-void Set_Road();
+class Road
+{
+public:
+	Road_Point point[MAX];
+	int point_number;//使用的点的个数
+	void Set_all_boundary();//设置所有边界
+	void Setpos(float size);//设置点
+	void initRoad();//构造路面
+};
+
+
